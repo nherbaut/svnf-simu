54bbbaafcc703e8386ee074c72133b2b9774d40c
Nicolas Herbaut <nherbaut@labri.fr>
2015-06-21 16:40:59 +0000

    eval2

--- eval2/eval2.sh
+++ eval2/eval2.sh
@@ -1,7 +1,6 @@
- for i in {1..10}; 
+for i in {1..16}
+do
+../build/svnf_simu --nGW=100 --nDw=1000 --mat=0.2 --popBitrate=1Gbps --cpBitrate=1Gbps  --vcs=1000 --vcv=1000 --ns3::TcpSocket::SegmentSize=14000 --ns3::TcpSocket::InitialCwnd=2 --as=10000000 --popDelay=25ms --cpDelay=50ms  --transTime=1  --gwUp=0.01 --pVidCount=200 --pStart=20 --pMeanArrTime=0.05 --PeakVcs=1000 --PeakVcv=0.1 --countBeforeCache=${i} --cpop=${i}  2>/dev/null 1>/dev/null &
+done
 
 
-do ../build/svnf_simu --nGW=20 --nDw=300 --mat=0.1 --popBitrate=1Gbps --cpBitrate=1Gbps  --vcs=1000 --vcv=100000 --ns3::TcpSocket::SegmentSize=14000 --ns3::TcpSocket::InitialCwnd=2 --as=10000000 --popDelay=25ms --cpDelay=50ms  --transTime=1  --gwUp=0.01 --pVidCount=100 --pStart=20 --pMeanArrTime=0.05 --PeakVcs=1000 --PeakVcv=0.1 --countBeforeCache=$i --cpop=$i;
-
-cut -c33-43 ../build/server-loads$i.csv > ../build/server-loads$i_cut.csv
-done;
